54997a54f598b67ef4727ad25cdb0010f3db2299
junwoo <rhffldkt0517@naver.com>
2023-05-03 03:49:59 +0900

    [Feat] Add Detail

--- expandableview/src/main/java/com/android/expandableview/MainActivity.kt
+++ expandableview/src/main/java/com/android/expandableview/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.android.expandableview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,13 +17,37 @@ class MainActivity : AppCompatActivity() {
 
         var expand = false
 
+        val expandDrawable = ContextCompat.getDrawable(this, R.drawable.gradient_expand)
+        val collapseDrawable = ContextCompat.getDrawable(this, R.drawable.gradient_collapse)
+        // 현재 이슈
+        // radient의 centerX, centerY값이 view의 visible gone 상태에
+        // 따라서 불규칙적으로 변하고 있음 그렇기 떄문에 이후 벡터로 할건지 아니면
+        // 근본적인 해결책을 찾아낼것인지 고민해봐야함 일단은 view의 상태 변화에도
+        // 약간 gradient의 차이가 발생하더라도 drawalbe을 교체하지 않는 이상태로 두겠음
+        // 혹시 리싸이클러뷰에서 적용하면 다르게 작동할지도 모름
+
+//        binding.clRoot.viewTreeObserver.addOnGlobalLayoutListener {
+//            if (expand)
+//                binding.clRoot.background = expandDrawable
+//            else
+//                binding.clRoot.background = collapseDrawable
+//        }
+
         binding.ivArrow.setOnClickListener {
-            expand = toggleLayout(!expand, binding.ivArrow, binding.clContent)
+            ExpandAnimation.toggleArrow(binding.ivArrow, !expand)
+            if (!expand) {
+                binding.clContent.visibility = View.VISIBLE
+            } else {
+                binding.clContent.visibility = View.GONE
+            }
+            expand = !expand
         }
     }
 
     fun toggleLayout(isExpanded: Boolean, view: View, layoutExpand: ConstraintLayout): Boolean {
         // 토글 레이아웃을 위한 애니메이션
+        // 열심히 만들어놨는데 실기기에서 깜빡임 이슈가 있어서
+        // constraintLayout에 android:animateLayoutChanges="true" 하나만 추가하면 됨..
         ExpandAnimation.toggleArrow(view, isExpanded)
         if (isExpanded) {
             ExpandAnimation.expand(layoutExpand)
