870d3a60a8f318183fc5af74a9dcb8ac421366c3
IslandofDream <rhffldkt0517@naver.com>
2023-05-28 21:36:14 +0900

    [Feat] 겉 테두리 색이 바뀌도록 TextInputlayout 사용

--- focusedittext/src/main/java/com/android/focusedittext/MainActivity.kt
+++ focusedittext/src/main/java/com/android/focusedittext/MainActivity.kt
@@ -2,6 +2,7 @@ package com.android.focusedittext
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.android.focusedittext.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.textInputLayout.editText?.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.textInputLayout.boxStrokeColor = ContextCompat.getColor(this, R.color.red)
+            } else {
+                binding.textInputLayout.boxStrokeColor = ContextCompat.getColor(this, R.color.black)
+            }
+        }
+
     }
 }
