2e0f0c34ccb1cb050e67a679dc9002d9190c0c97
IslandofDream <rhffldkt0517@naver.com>
2023-05-30 23:00:44 +0900

    [Feat] TextInputLayout 조정 및 겉 테두리 색 조정

--- focusedittext/src/main/java/com/android/focusedittext/MainActivity.kt
+++ focusedittext/src/main/java/com/android/focusedittext/MainActivity.kt
@@ -14,13 +14,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textInputLayout.editText?.setOnFocusChangeListener { _, hasFocus ->
-            if (hasFocus) {
-                binding.textInputLayout.boxStrokeColor = ContextCompat.getColor(this, R.color.red)
-            } else {
-                binding.textInputLayout.boxStrokeColor = ContextCompat.getColor(this, R.color.black)
-            }
-        }
+//        binding.textInputLayout.editText?.setOnFocusChangeListener { _, hasFocus ->
+//            if (hasFocus) {
+//                binding.textInputLayout.boxStrokeColor = ContextCompat.getColor(this, R.color.red)
+//            } else {
+//                binding.textInputLayout.boxStrokeColor = ContextCompat.getColor(this, R.color.black)
+//            }
+//        }
+//
+//        binding.textInputLayout2.editText?.setOnFocusChangeListener { _, hasFocus ->
+//            if (hasFocus) {
+//                binding.textInputLayout2.boxStrokeColor = ContextCompat.getColor(this, R.color.red)
+//            } else {
+//                binding.textInputLayout2.boxStrokeColor = ContextCompat.getColor(this, R.color.black)
+//            }
+//        }
 
     }
 }
